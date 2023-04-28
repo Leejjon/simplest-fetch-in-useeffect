fe3041aaf7a2349685b217af91f0928b0c9c3f7b
Leejjon <leejjon@gmail.com>
2023-04-29 01:35:54 +0200

    Now applied loaders using createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
-import Matches from "./pages/Matches";
-import Matches2 from "./pages/Matches2";
-import {getMatchesFromApi} from "./api/ApiCall";
+import {Link, Outlet} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Link to="/">Home</Link><br/>
-        <Link to="/matches">Matches</Link><br/>
-        <Link to="/matches2">Matches2</Link><br/>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/matches" element={<Matches />} loader={ async () => { await getMatchesFromApi(2021)} } />
-          <Route path="/matches2" element={<Matches2 />} loader={ async () => { await getMatchesFromApi(2021)} } />
-        </Routes>
-      </BrowserRouter>
+      <Link to="/">Home</Link><br/>
+      <Link to="/matches">Matches</Link><br/>
+      <Link to="/matches2">Matches2</Link><br/>
+      <Outlet/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from "./App";
+import Matches from "./pages/Matches";
+import Matches2 from "./pages/Matches2";
+import {getMatchesFromApi} from "./api/ApiCall";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                path: "/matches",
+                element: <Matches/>,
+                loader: async () => { return await getMatchesFromApi(2021)}
+            },
+            {
+                path: "/matches2",
+                element: <Matches2 />,
+                loader: async () => { return await getMatchesFromApi(2021)}
+            }
+        ]
+    }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  // <React.StrictMode>
-    <App />
-  //</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
