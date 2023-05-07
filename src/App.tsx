75492024cb2b7ff4ad31797efb6e566cf1d74808
Leejjon <leejjon@gmail.com>
2023-05-07 19:19:31 +0200

    Added error handling.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import {Match} from "./model/Match";
 import {getMatchesFromApi} from "./api/ApiCall";
 
 function App() {
+  const [error, setError] = useState<string | undefined>(undefined);
   const [matches, setMatches] = useState<Array<Match>>([]);
   useEffect(() => {
     if (matches.length === 0) {
       getMatchesFromApi(2021)
-          .then(matches => setMatches(matches))
+          .then(matches => {
+            setMatches(matches);
+            setError(undefined);
+          })
+          .catch((e) => setError("Could not fetch the matches."));
     }
   }, [matches, setMatches]);
+  if (error) {
+    return (
+        <div className="App">{error}</div>
+    );
+  }
   return (
     <div className="App">
       <ul>
